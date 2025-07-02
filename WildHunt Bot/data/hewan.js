6846bd995bc564c58a1bab53a4f363b31a415ea3
Siapa lu? <siapalubre@gmail.com>
2025-07-02 21:45:02 +0800

    Add files via upload

---
+++ WildHunt Bot/data/hewan.js
@@ -0,0 +1,11 @@
+
+module.exports = [
+  { nama: '🐇 Kelinci', hadiah: 50, rarity: 'umum' },
+  { nama: '🦌 Rusa', hadiah: 100, rarity: 'umum' },
+  { nama: '🐗 Babi Hutan', hadiah: 120, rarity: 'umum' },
+  { nama: '🐻 Beruang', hadiah: 250, rarity: 'langka' },
+  { nama: '🦅 Elang', hadiah: 200, rarity: 'langka' },
+  { nama: '🐅 Harimau', hadiah: 500, rarity: 'epik' },
+  { nama: '🐘 Gajah', hadiah: 600, rarity: 'epik' },
+  { nama: '🐉 Naga Hutan', hadiah: 1000, rarity: 'legendaris' }
+];
---
+++ WildHunt Bot/data/ikan.js
@@ -0,0 +1,9 @@
+
+module.exports = [
+  { nama: '🐟 Ikan Kecil', hadiah: 50, rarity: 'umum' },
+  { nama: '🐠 Ikan Hias', hadiah: 100, rarity: 'umum' },
+  { nama: '🦑 Cumi-Cumi', hadiah: 150, rarity: 'langka' },
+  { nama: '🦈 Hiu', hadiah: 500, rarity: 'epik' },
+  { nama: '🐉 Ikan Naga Laut', hadiah: 1000, rarity: 'legendaris' },
+  { nama: '🪤 Sepatu Tua', hadiah: 0, zonk: true }
+];
