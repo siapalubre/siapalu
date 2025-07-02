0a71bf6d2bd9e647240f794bdf258ce8aeecb899
Siapa lu? <siapalubre@gmail.com>
2025-07-02 21:44:39 +0800

    Create harta.js

---
+++ WildHunt Bot/data/harta.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { nama: '🪙 Koin Emas', hadiah: 100, rarity: 'umum' },
+  { nama: '💎 Permata', hadiah: 300, rarity: 'langka' },
+  { nama: '🏺 Artefak Kuno', hadiah: 500, rarity: 'epik' },
+  { nama: '👑 Mahkota Raja', hadiah: 1000, rarity: 'legendaris' },
+  { nama: '📦 Peti Kosong', hadiah: 0, zonk: true }
+];
